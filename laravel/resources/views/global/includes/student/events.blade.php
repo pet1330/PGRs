5fe34804cded9c1887dd3b58b743d0e03c839129
Jamie Lord <jamie@lord.technology>
2015-07-06 16:11:27 +0100

    Events blade

---
+++ laravel/resources/views/global/includes/student/events.blade.php
@@ -0,0 +1,144 @@
+<div class="panel panel-default">
+    <div class="panel-heading"><i class="fa fa-clock-o fa-fw"></i> Events</div>
+    <!-- /.panel-heading -->
+    <div class="panel-body" style="padding: 0 15px 0 0">
+        @if (count($events->all()) > 0 )
+        <ul class="timeline">
+            @foreach ($events->all() as $event)
+            <li>
+                <div class="timeline-badge"><i class="fa fa-check"></i>
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">{{ $event->title }}@if ($event->staff_id != NULL)<small> by {{ $event->staff->user->full_name }}</small>@elseif ($event->created_by == 'System') <span class="label label-info pull-right">Automated event</span>@else<small> by{{ $event->created_by }}</small>@endif</h4>
+                        <p><small class="text-muted"><i class="fa fa-clock-o"></i> {{ date('F d, Y', strtotime($event->created)) }} at {{ date("G:H",strtotime($event->created)) }}</small>
+                        </p>
+                    </div>
+                    <div class="timeline-body"><p>{{ $event->content }}</p></div>
+                    <div class="timeline-footer">
+                        <div class="btn-group">
+                            <a class="btn btn-default" href="{{ action('EventsController@edit', ['enrolment' => $student->enrolment, 'id' => $event->id]) }}">Edit</a>
+                        </div>
+                        <div class="btn-group">
+                            <form action="{{ action('EventsController@destroy', ['enrolment' => $student->enrolment, 'id' => $event->id]) }}" method="POST">
+                            <input type="hidden" name="_method" value="DELETE">
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            @endforeach
+        </ul>
+        @else
+        <div style="padding: 15px">There are no events for this student.</div>
+        @endif
+    </div>
+    <!-- /.panel-body -->
+    <div class="panel-footer">
+        <div class="btn-group">
+            <a class="btn btn-default" href="{{ action('EventsController@create', ['enrolment' => $student->enrolment]) }}">Add new event</a>
+        </div>
+    </div>
+</div>
+
+{{-- <li>
+                <div class="timeline-badge"><i class="fa fa-check"></i>
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                        <p><small class="text-muted"><i class="fa fa-clock-o"></i> 11 hours ago</small>
+                        </p>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero laboriosam dolor perspiciatis omnis exercitationem. Beatae, officia pariatur? Est cum veniam excepturi. Maiores praesentium, porro voluptas suscipit facere rem dicta, debitis.</p>
+                    </div>
+                </div>
+            </li>
+            <li class="timeline-inverted">
+                <div class="timeline-badge warning"><i class="fa fa-credit-card"></i>
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolorem quibusdam, tenetur commodi provident cumque magni voluptatem libero, quis rerum. Fugiat esse debitis optio, tempore. Animi officiis alias, officia repellendus.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maiores odit qui est tempora eos, nostrum provident explicabo dignissimos debitis vel! Adipisci eius voluptates, ad aut recusandae minus eaque facere.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge danger"><i class="fa fa-bomb"></i>
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus numquam facilis enim eaque, tenetur nam id qui vel velit similique nihil iure molestias aliquam, voluptatem totam quaerat, magni commodi quisquam.</p>
+                    </div>
+                </div>
+            </li>
+            <li class="timeline-inverted">
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates est quaerat asperiores sapiente, eligendi, nihil. Itaque quos, alias sapiente rerum quas odit! Aperiam officiis quidem delectus libero, omnis ut debitis!</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge info"><i class="fa fa-save"></i>
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis minus modi quam ipsum alias at est molestiae excepturi delectus nesciunt, quibusdam debitis amet, beatae consequuntur impedit nulla qui! Laborum, atque.</p>
+                        <hr>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-gear"></i>  <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a>
+                                </li>
+                                <li><a href="#">Another action</a>
+                                </li>
+                                <li><a href="#">Something else here</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fuga odio quibusdam. Iure expedita, incidunt unde quis nam! Quod, quisquam. Officia quam qui adipisci quas consequuntur nostrum sequi. Consequuntur, commodi.</p>
+                    </div>
+                </div>
+            </li>
+            <li class="timeline-inverted">
+                <div class="timeline-badge success"><i class="fa fa-graduation-cap"></i>
+                </div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt obcaecati, quaerat tempore officia voluptas debitis consectetur culpa amet, accusamus dolorum fugiat, animi dicta aperiam, enim incidunt quisquam maxime neque eaque.</p>
+                    </div>
+                </div>
+            </li> --}}
