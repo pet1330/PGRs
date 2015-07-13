8115600f682e7d7f13d863dbdd03349ae7d2831f
Jamie Lord <jamie@lord.technology>
2015-07-13 14:37:59 +0100

    Installed reportico

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -145,6 +145,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Reportico\Reportico\ReporticoServiceProvider::class,
+
     ],
 
     /*
