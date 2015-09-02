55a72496d915915ab86ed3592aa53dc21e31f8a6
Jamie Lord <jamie@lord.technology>
2015-09-02 18:04:40 +0100

    New layout for student show

--- laravel/resources/views/entities/students/show.blade.php
+++ laravel/resources/views/entities/students/show.blade.php
@@ -3,7 +3,7 @@
 {{ $student->user->full_name }} Overview
 @endsection
 @section('page_title')
-{{ $student->user->full_name }} <small>{{ $student->enrolment }}</small> @foreach($student->user->roles as $role) <span class="label label-info">{{ $role->display_name }}</span>@endforeach @if($student->end && strtotime($student->end) < time()) <span class="label label-warning">Completed study</span>@elseif(strtotime($student->start) > time()) <span class="label label-success">Future student</span>@else <span class="label label-success">Year {{ $student->current_year }}</span>@endif @if($student->time_since_last_gs5) <span class="label label-primary">Last GS5 {{ $student->time_since_last_gs5 }}</span>@endif @if(count($upcoming_events->all()) == 1)<span class="label label-warning">1 Upcoming event</span>@elseif(count($upcoming_events->all()) > 1)<span class="label label-warning">{{ count($upcoming_events->all()) }} Upcoming events</span>@endif @if($student->user->locked == '1')<span class="label label-danger">Account login disabled</span>@endif
+{{ $student->user->full_name }} <small>{{ $student->enrolment }}</small> @foreach($student->user->roles as $role) <span class="label label-info">{{ $role->display_name }}</span>@endforeach @if($student->end && strtotime($student->end) < time()) <span class="label label-warning">Completed {{ $student->award->name }} study</span>@elseif(strtotime($student->start) > time()) <span class="label label-success">Future {{ $student->award->name }} student</span>@else <span class="label label-success">Year {{ $student->current_year }} {{ $student->award->name }}</span>@endif @if($student->time_since_last_gs5) <span class="label label-primary">Last GS5 {{ $student->time_since_last_gs5 }}</span>@endif @if(count($upcoming_events->all()) == 1)<span class="label label-warning">1 Upcoming event</span>@elseif(count($upcoming_events->all()) > 1)<span class="label label-warning">{{ count($upcoming_events->all()) }} Upcoming events</span>@endif @if($student->user->locked == '1')<span class="label label-danger">Account login disabled</span>@endif
 @endsection
 @section('table_name', 'supervisors')
 @section('content')
@@ -15,19 +15,39 @@
         @endif
     </div>
     <div class="row">
-        <div class="col-lg-5 col-md-12">
-            @include('global.includes.student.profile')
-            @include('global.includes.student.absences')
-        </div>
-        <div class="col-lg-7 col-md-12">
-            @if(count($upcoming_events->all()) > 0)
-            @include('global.includes.student.upcoming_events')
-            @endif
-            @include('global.includes.student.supervisors')
-            @include('global.includes.student.events')
-            @if (Entrust::can('can_view_any_student_history') || (Entrust::can('can_view_my_students_history') && Auth::user()->staff->isMyStudent($student->id)))
-            @include('global.includes.student.history')
-            @endif
+        <div class="panel-body" style="padding-top: 0px">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#events_tab" data-toggle="tab" aria-expanded="true">Events &amp; Supervisors</a>
+                </li>
+                <li class=""><a href="#profile_tab" data-toggle="tab" aria-expanded="false">Profile, History &amp; Absences</a>
+                </li>
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div class="tab-pane fade active in" id="events_tab">
+                    <div class="col-lg-7 col-md-12">
+                        @if(count($upcoming_events->all()) > 0)
+                        @include('global.includes.student.upcoming_events')
+                        @endif
+                        @include('global.includes.student.events')
+                    </div>
+                    <div class="col-lg-5 col-md-12">
+                        @include('global.includes.student.supervisors')
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="profile_tab">
+                    <div class="col-lg-5 col-md-12">
+                        @include('global.includes.student.profile')
+                    </div>
+                    <div class="col-lg-7 col-md-12">
+                        @if (Entrust::can('can_view_any_student_history') || (Entrust::can('can_view_my_students_history') && Auth::user()->staff->isMyStudent($student->id)))
+                        @include('global.includes.student.history')
+                        @endif
+                        @include('global.includes.student.absences')
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
