eba83681a61205e8a7d25974118b3b46c170ce34
Jamie Lord <jamie@lord.technology>
2015-09-02 13:37:20 +0100

    Fix administrators dashboard

--- laravel/resources/views/admin/pages/dashboard.blade.php
+++ laravel/resources/views/admin/pages/dashboard.blade.php
@@ -3,29 +3,27 @@
 @section('content')
 <div class="row">
 	<div class="col-lg-6 col-md-6">
-		<div class="panel panel-primary">
+		<div class="panel panel-primary" onclick="location.href='{{ action('StudentsController@index') }}';">
 			<div class="panel-heading">
 				<div class="row">
 					<div class="col-xs-3">
 						<i class="fa fa-graduation-cap fa-5x"></i>
 					</div>
 					<div class="col-xs-9 text-right">
-					<div class="huge">Students</div>
-					<div>Student management</div>
+						<div class="huge">Students</div>
+						<div>Student management</div>
 					</div>
 				</div>
 			</div>
-			<a href="{{ action('StudentsController@index') }}">
-				<div class="panel-footer">
-					<span class="pull-left">View Details</span>
-					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-					<div class="clearfix"></div>
-				</div>
-			</a>
+			<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</div>
 		</div>
 	</div>
 	<div class="col-lg-6 col-md-6">
-		<div class="panel panel-primary">
+		<div class="panel panel-primary" onclick="location.href='{{ action('StaffController@index') }}';">
 			<div class="panel-heading">
 				<div class="row">
 					<div class="col-xs-3">
@@ -37,17 +35,15 @@
 					</div>
 				</div>
 			</div>
-			<a href="{{ action('StaffController@index') }}">
-				<div class="panel-footer">
-					<span class="pull-left">View Details</span>
-					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-					<div class="clearfix"></div>
-				</div>
-			</a>
+			<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</div>
 		</div>
 	</div>
 	<div class="col-lg-6 col-md-6">
-		<div class="panel panel-primary">
+		<div class="panel panel-primary" onclick="location.href='/events';">
 			<div class="panel-heading">
 				<div class="row">
 					<div class="col-xs-3">
@@ -59,17 +55,15 @@
 					</div>
 				</div>
 			</div>
-			<a href="#">
-				<div class="panel-footer">
-					<span class="pull-left">View Details</span>
-					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-					<div class="clearfix"></div>
-				</div>
-			</a>
+			<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</div>
 		</div>
 	</div>
 	<div class="col-lg-6 col-md-6">
-		<div class="panel panel-primary">
+		<div class="panel panel-primary" onclick="location.href='/reports';">
 			<div class="panel-heading">
 				<div class="row">
 					<div class="col-xs-3">
@@ -81,13 +75,11 @@
 					</div>
 				</div>
 			</div>
-			<a href="/reports">
-				<div class="panel-footer">
-					<span class="pull-left">View Details</span>
-					<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-					<div class="clearfix"></div>
-				</div>
-			</a>
+			<div class="panel-footer">
+				<span class="pull-left">View Details</span>
+				<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</div>
 		</div>
 	</div>
 </div>
