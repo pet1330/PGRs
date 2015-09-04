79cb720ec54bdbf778126325bf88520784c5f330
Jamie Lord <jamie@lord.technology>
2015-09-04 21:36:31 +0100

    Show student profile picture in header

--- laravel/resources/views/admin/layouts/default.blade.php
+++ laravel/resources/views/admin/layouts/default.blade.php
@@ -11,7 +11,14 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12">
+                        @if(isset($__env->getSections()['profile_image']))
+                        <div style="float: left; margin-bottom: 1em;margin-right: 1em; margin-top: 1em;">
+                            @yield('profile_image')
+                        </div>
                         <h1 class="page-header">@if (isset($__env->getSections()['page_title']))@yield('page_title')@else @yield('title')@endif</h1>
+                        @else
+                        <h1 class="page-header">@if (isset($__env->getSections()['page_title']))@yield('page_title')@else @yield('title')@endif</h1>
+                        @endif
                         @yield('content')
                     </div>
                     <!-- /.col-lg-12 -->
--- laravel/resources/views/entities/students/show.blade.php
+++ laravel/resources/views/entities/students/show.blade.php
@@ -5,6 +5,11 @@
 @section('page_title')
 {{ $student->user->full_name }} <small>{{ $student->enrolment }}</small> @foreach($student->user->roles as $role) <span class="label label-info">{{ $role->display_name }}</span>@endforeach @if($student->end && strtotime($student->end) < time()) <span class="label label-warning">Completed {{ $student->award->name }} study</span>@elseif(strtotime($student->start) > time()) <span class="label label-success">Future {{ $student->award->name }} student</span>@else <span class="label label-success">Year {{ $student->current_year }} {{ $student->award->name }}</span>@endif @if($student->time_since_last_gs5) <span class="label label-primary">Last GS5 {{ $student->time_since_last_gs5 }}</span>@endif @if(count($upcoming_events->all()) == 1)<span class="label label-warning">1 Upcoming event</span>@elseif(count($upcoming_events->all()) > 1)<span class="label label-warning">{{ count($upcoming_events->all()) }} Upcoming events</span>@endif @if($student->user->locked == '1')<span class="label label-danger">Account login disabled</span>@endif
 @endsection
+@if($student->user->image)
+    @section('profile_image')
+        <img src="/userImages/{{ $student->user->image }}" alt="{{ $student->user->full_name }}" class="img-thumbnail" style="max-width: 100px">
+    @endsection
+@endif
 @section('table_name', 'supervisors')
 @section('content')
 <div class="container-fluid">
@@ -20,7 +25,9 @@
             <ul class="nav nav-tabs">
                 <li class="active"><a href="#events_tab" data-toggle="tab" aria-expanded="true">Events &amp; Supervisors</a>
                 </li>
-                <li class=""><a href="#profile_tab" data-toggle="tab" aria-expanded="false">Profile, History &amp; Absences</a>
+                <li class=""><a href="#history_tab" data-toggle="tab" aria-expanded="false">History &amp; Absences</a>
+                </li>
+                <li class=""><a href="#profile_tab" data-toggle="tab" aria-expanded="false">Profile</a>
                 </li>
             </ul>
             <!-- Tab panes -->
@@ -36,16 +43,14 @@
                         @include('global.includes.student.supervisors')
                     </div>
                 </div>
+                <div class="tab-pane fade" id="history_tab">
+                    @if (Entrust::can('can_view_any_student_history') || (Entrust::can('can_view_my_students_history') && Auth::user()->staff->isMyStudent($student->id)))
+                    @include('global.includes.student.history')
+                    @endif
+                    @include('global.includes.student.absences')
+                </div>
                 <div class="tab-pane fade" id="profile_tab">
-                    <div class="col-lg-5 col-md-12">
-                        @include('global.includes.student.profile')
-                    </div>
-                    <div class="col-lg-7 col-md-12">
-                        @if (Entrust::can('can_view_any_student_history') || (Entrust::can('can_view_my_students_history') && Auth::user()->staff->isMyStudent($student->id)))
-                        @include('global.includes.student.history')
-                        @endif
-                        @include('global.includes.student.absences')
-                    </div>
+                    @include('global.includes.student.profile')
                 </div>
             </div>
         </div>
